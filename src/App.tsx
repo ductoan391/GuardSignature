c616f7581efe2c1203d0c9e267d6041c66ba05a6
Duc Toan <ductoan391@gmail.com>
2022-01-11 09:28:12 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Analyze from './components/Analyze/Analyze';
+import Contract from './components/Contract/Contract';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import HeaderSlide from './components/HeaderSlide/HeaderSlide';
+import Item1 from './components/Item1/Item1';
+import Item2 from './components/Item2/Item2';
+import Now from './components/Now/Now';
+import Protect from './components/Proctect/Protect';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <HeaderSlide />
+      <Contract />
+      <Now />
+      <Item1 />
+      <Item2 />
+      <Analyze />
+      <Protect />
+      <Footer />
     </div>
   );
 }
